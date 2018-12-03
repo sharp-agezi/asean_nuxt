39bad5138cf9aea9e8cfc2ddae057a304c715699
sharp-agezi <1150211686@qq.com>
2018-12-03 10:59:41 +0800

    bug修改

--- plugins/plugins.js
+++ plugins/plugins.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
-// import iView from 'iview';
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import vueLazyload from "vue-lazyload"
-
-// import 'iview/dist/styles/iview.css';
-// import '../assets/style/index.scss';
-// import 'swiper/dist/css/swiper.css';
-// import '../assets/iconfont/iconfont.css';
-// import '../assets/iconfont/iconfont.js';
-// Vue.use(iView);
 Vue.use(VueAwesomeSwiper);
-Vue.use(vueLazyload);
+
 
 
